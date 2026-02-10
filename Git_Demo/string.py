81a6d648de9964cc66e1a1bd88a13efbc6b91fee
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-02-10 23:55:54 +0530

    Create string.py

---
+++ Git_Demo/string.py
@@ -0,0 +1,8 @@
+def string(str1,str2):
+    print("Length of string: ",len(str1))
+    print("Convert string into uppercase: ",str1.upper())
+    print("Convert string into lowercase: ",str1.lower())
+  
+str1=input("Enter a string 1: ")
+str2=input("Enter string 2: ")
+string(str1,str2)
