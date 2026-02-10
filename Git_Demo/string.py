37f726a8bf7429732bfdc20a034c5c566a7f97b2
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-02-10 23:58:57 +0530

    Update string.py

--- Git_Demo/string.py
+++ Git_Demo/string.py
@@ -1,8 +1,12 @@
+#String operations
+
 def string(str1,str2):
     print("Length of string: ",len(str1))
     print("Convert string into uppercase: ",str1.upper())
     print("Convert string into lowercase: ",str1.lower())
-  
+    concatenated_string = str1 + str2
+    print("Concatenate 2 strings: ", concatenated_string)
+    
 str1=input("Enter a string 1: ")
 str2=input("Enter string 2: ")
 string(str1,str2)
