179e1e539877e652a98b1d5fbcb103781c9d05f8
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-02-13 23:53:54 +0530

    add code in asce.py

--- Git_Demo/asce_order.py
+++ Git_Demo/asce_order.py
@@ -1,1 +1,15 @@
+#List in ascending order...
 
+def list(l):
+  l.sort()
+  print("Array elements in ascending order: ",l)
+    
+n=int(input("enter number of elemets added in the list: "))
+l=[]
+print("enter ",n, " elements in list")
+for i in range(n):
+  x=int(input())
+  l.append(x)
+
+print("Original List: ",l)
+list(l)
