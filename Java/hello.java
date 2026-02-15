b8d2a8647adb73cf226a04d74994f8d160cb49cd
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-02-15 22:13:13 +0530

    Update hello.java

--- Java/hello.java
+++ Java/hello.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class hello
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Welcome in the  Java world");
+    Scanner sc=new Scanner(System.in);
+    
+    System.out.println("What is Your name: ");
+    String name=sc.nextLine();
+
+    System.out.println("What's your age: ");
+    int age=sc.nextInt();
+
+    System.out.println("My name is "+name+ "  and my age is "+age);
+
+  }
+}
