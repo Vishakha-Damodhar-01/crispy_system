02d6700f05cfb413fe5f1aa9fe69380d90623f03
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-02-16 23:46:30 +0530

    Update input.java

--- Java/input.java
+++ Java/input.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class hello
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Arithmetic Operations using user Input");
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter First value: ");
+    int a=sc.nextInt();
+
+    System.out.println("Enter second value: ");
+    int b=sc.nextInt();
+
+    System.out.println("Addition: "+(a+b));
+    System.out.println("Subtraction: "+(a-b));
+    System.out.println("Multiplication: "+(a*b));
+    System.out.println("Division: "+(a/b));
+    System.out.println("Modulus: "+(a%b));
+  }
+}
