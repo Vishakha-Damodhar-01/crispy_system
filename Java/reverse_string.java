e6712b334d7a90e25a252d31d414af5c808b676d
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-02-25 16:25:50 +0530

    Update reverse_string.java

--- Java/reverse_string.java
+++ Java/reverse_string.java
@@ -1,1 +1,18 @@
+public class reverse_string
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    String str=sc.nextLine();
+    System.out.println("Original String: "+str);
 
+    StringBuilder reverse_string=new Stringbuilder(str);
+    reverse_string.reverse();
+
+    String rev_str=reverse_string.ToString();
+
+    System.out.println("Rverse string: "+rev_str);
+  }
+}
+  
+    
