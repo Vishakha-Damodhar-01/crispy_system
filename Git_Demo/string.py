b89ce8c6993f302838551d8eb305db4efde1e486
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-02-28 23:34:40 +0530

    Update string.py

--- Git_Demo/string.py
+++ Git_Demo/string.py
@@ -1,5 +1,5 @@
 #String operations
-
+# 1. Concatenate the 2 strings
 def string(str1,str2):
     print("Length of string: ",len(str1))
     print("Convert string into uppercase: ",str1.upper())
@@ -10,3 +10,10 @@ def string(str1,str2):
 str1=input("Enter a string 1: ")
 str2=input("Enter string 2: ")
 string(str1,str2)
+
+# 2. Calculate lenghth of a string and reverse it
+
+str=input("Enter a String: ")
+print("Length of a entered string: ",len(str))
+reverse=str[::-1]   
+print("Reverse string : ",reverse)
