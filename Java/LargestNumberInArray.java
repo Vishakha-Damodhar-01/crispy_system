5f2eff8b35f916206fe01272114a39937d90da79
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-01 23:55:08 +0530

    Update LargestNumberInArray.java

--- Java/LargestNumberInArray.java
+++ Java/LargestNumberInArray.java
@@ -1,1 +1,20 @@
 
+public class LargestNumberInArray
+{
+    public static void main(String[] args)
+    {
+        int[] num={5,10,5,88,4,65};
+        int largest=num[0];
+
+        for (int i=0;i<num.length;i++)
+        {
+            if(num[i]>largest)
+            {
+                largest=num[i];
+                
+            }
+        }
+
+        System.out.println("Largest element in array: "+largest);
+    }
+}
