a9d86de06fa12311cf88c35bb5c6d3a84a9a4eef
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-02 20:21:23 +0530

    Update largestNumInarray.py

--- Git_Demo/largestNumInarray.py
+++ Git_Demo/largestNumInarray.py
@@ -1,1 +1,23 @@
+#1. Largest Number from array;:
 
+def largestNum(l):
+  largest=l[0]
+  for i in l:
+    if i>largest:
+      largest=i
+  print(largest)
+
+try:
+    n = int(input("Enter the number of elements added in the list: "))
+    l = []
+    print("Enter", n, "values in array")
+    for i in range(n):
+        x = int(input(f"Value {i+1}: "))
+        l.append(x)
+    
+    print("Original List:", l)
+    # Call function
+    largestNum(l)
+
+except ValueError:
+    print("Invalid input. Please enter only integers.")
