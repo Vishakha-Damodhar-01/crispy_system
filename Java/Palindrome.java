e1bbe21fe92564b9057c5a9570ea4fc7c836b81c
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-04 22:51:24 +0530

    Wrote code in Palindrome.java

--- Java/Palindrome.java
+++ Java/Palindrome.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class reverse_string 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str=sc.nextLine();
+        // String same =str;
+        StringBuilder reverse_string=new StringBuilder(str);
+
+        reverse_string.reverse();
+
+        String reverse_str=reverse_string.toString();
+        System.out.println("Original String: "+str);
+        System.out.println("Reversed string: "+reverse_str);
+
+        if(str.equals(reverse_str))
+        {
+            System.out.println("The string is palindrome");
+
+        }
+        else
+        {
+            System.out.println("The string is not palindrome");
+        }
+    }
+}
