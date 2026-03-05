fa71fd81c6f34367eb4ae32dee19e54017b20e21
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-05 16:50:38 +0530

    Update SumOfDigitOfNum.py

--- Git_Demo/SumOfDigitOfNum.py
+++ Git_Demo/SumOfDigitOfNum.py
@@ -1,1 +1,11 @@
 
+def Sumofdigitofnum(n):
+    sum=0
+    while n>0:
+        p=n%10
+        sum=sum+p
+        n=n//10
+    print("Sum of digit of anumber: ",sum)
+
+n=int(input("Enter a number: "))
+Sumofdigitofnum(n)
