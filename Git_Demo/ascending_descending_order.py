9773aef104f74773f365e1e7378c04bb187ea750
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-08 23:43:09 +0530

    add code pf decending order  and rename asce_order.py to ascending_descending_order.py

--- Git_Demo/ascending_descending_order.py
+++ Git_Demo/ascending_descending_order.py
@@ -1,8 +1,12 @@
 #List in ascending order...
 
-def list(l):
+def acending(l):
   l.sort()
   print("Array elements in ascending order: ",l)
+
+def decending(l):
+  l.sort(reverse=true)
+  print("array elements in decending order: ",l)
     
 n=int(input("enter number of elemets added in the list: "))
 l=[]
@@ -12,4 +16,5 @@ for i in range(n):
   l.append(x)
 
 print("Original List: ",l)
-list(l)
+acending(l)
+decending(l)
