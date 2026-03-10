4fedf3539c23456f1e00723f7b74c87b83c47fa8
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-10 22:50:13 +0530

    Create smallestNumInArray.py

---
+++ Git_Demo/smallestNumInArray.py
@@ -0,0 +1,10 @@
+
+def smallestNum(l):
+  pass
+
+try:
+  n=int(input("Enter a number of elements add in the list: "))
+  l=[]
+  for i in l:
+    l.sort()
+    print("Smallest element in array is : "+ l[0])
