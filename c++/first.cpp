0fda54b73f0cf008b2fc5f3637db08089b492734
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-11 22:40:37 +0530

    write code in first.cpp

--- c++/first.cpp
+++ c++/first.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter value of a" << endl;
+    cin>>a;
+    cout<<"Enter value of b"<< endl;
+    cin>>b;
+    int c;
+    c=a+b;
+    cout<<"Addition of 2 numbers is: "<<c<<endl;
+    return 0;
+}
+
+// output:
+// Enter value of a
+// 5
+// Enter value of b
+// 10
+// Addition of 2 numbers is: 15
