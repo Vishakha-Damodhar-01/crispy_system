c702a5ea8956e526284e0b86534e82f388be1ac3
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-13 17:19:39 +0530

    added code in  basic.cpp

--- c++/basic.cpp
+++ c++/basic.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    cout<<"Enter a number: "<<endl;
+    int n, sq,cb;
+    
+    cin>>n;
+    sq=n*n;
+    cb=n*n*n;
+    cout<<"The square of "<<n<<"is: "<<sq<<endl;
+    cout<<"The cube of "<<n<<"is: "<<cb<<endl;
+    
+    return 0;
+}
