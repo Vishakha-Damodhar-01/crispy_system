f1bc318a15e622100eba936af54383a63aaedaf5
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-14 22:23:55 +0530

    Update prime.cpp

--- c++/prime.cpp
+++ c++/prime.cpp
@@ -1,1 +1,26 @@
+// Write a cpp program for prime number
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i,count=0;
+    cout<<"enter a number: "<<endl;
+    cin>>n;
+    for(i=2;i<n;i++)
+    {
+        count=1;
+        break;
+    }
+    
+    if(count==0)
+    {
+        cout<<n<< " is a Prime number ";
+    }
+    else
+    {
+        cout<<n<<" is not a prime number ";
+    }
+    return 0;
+}
