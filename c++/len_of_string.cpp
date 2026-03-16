647ff373149a63898fbc14bdb0bd4ae0ef384016
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-16 23:15:13 +0530

    Update len_of_string.cpp

--- c++/len_of_string.cpp
+++ c++/len_of_string.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    std::string text = "Hello Guys ! welcome .....";
+
+    // Using length()
+    std::cout << "The length of the string is: " << text.length() << std::endl; 
+
+    // Using size()
+    std::cout << "The size of the string is: " << text.size() << std::endl;    
+    // Output: 13
+
+    return 0;
+}
