1db51c50478b6bca3d5e2b9e102222ef7cf606e7
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-17 23:03:45 +0530

    add code inside factorial.cpp

--- c++/factorial.cpp
+++ c++/factorial.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    long fact=1;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    cout<<" The factorial of "<<n<<" is: "<<fact;
+    return 0;
+}
