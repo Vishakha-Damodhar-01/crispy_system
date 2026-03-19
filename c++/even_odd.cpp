f62363f0aa3c51f1ee13158019226c0dd4d9de00
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-19 22:18:11 +0530

    Update even_odd.cpp

--- c++/even_odd.cpp
+++ c++/even_odd.cpp
@@ -1,1 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+  int n;
+  cout<<"Enter a number: "<<endl;
+  cin>>n;
+  if (n%2==0)
+  {
+    cout<<n<<" is even number";
+  }
+  else
+  {
+    cout<<n<<" is odd number";
+  }
+  return 0;
+}
