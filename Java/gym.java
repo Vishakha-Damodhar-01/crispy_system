b7138e6933548a664221fdbc9ddf2a8c20cf37f0
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-21 21:46:34 +0530

    add main code in gym.java

--- Java/gym.java
+++ Java/gym.java
@@ -9,3 +9,45 @@
 // # Takes the membership duration (in months) as input.
 // # Displays the corresponding membership cost.
 // # If the user enters a duration other than the specified plans, the program should display an error message:"Invalid Input"
+
+
+import java.util.*;
+
+public class tcs
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter a month: ");
+            int month=sc.nextInt();
+
+            switch(month)
+            {
+                case 1:
+                    System.out.println("3000");
+                    break;
+                case 3:
+                    System.out.println("6000");
+                    break;
+                case 6:
+                    System.out.println("9000");
+                    break;
+                case 9:
+                    System.out.println("12000");
+                    break;
+                case 12:
+                    System.out.println("15000");
+                    break;
+                default:
+                    System.out.println("Invalid input, for this months no plan is there in gym");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Please enter an positive integer value");
+        }
+
+    }
+}
