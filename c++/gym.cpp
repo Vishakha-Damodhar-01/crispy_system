957ff87a67c64335966abbdc98b56642c41e4027
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-23 22:26:19 +0530

    Update gym.cpp

--- c++/gym.cpp
+++ c++/gym.cpp
@@ -1,5 +1,15 @@
+// # Q1: Gym Membership Problem Statement
+// # The gym offers membership plans for specific durations. The cost of each plan is as follows:
+// # 1-month membership → ₹2000
+// # 3-month membership → ₹5000
+// # 6-month membership → ₹9000
+// # 9-month membership → ₹12000
+// # 12-month membership → ₹15000
+// # Write a program that:
+// # Takes the membership duration (in months) as input.
+// # Displays the corresponding membership cost.
+// # If the user enters a duration other than the specified plans, the program should display an error message:"Invalid Input"
 
-// Online C++ compiler to run C++ program online
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
