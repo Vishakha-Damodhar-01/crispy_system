850fa559fb81e1bafde7468261ec00f6448ab90a
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-25 12:56:53 +0530

    Update space_separated_array.java

--- Java/space_separated_array.java
+++ Java/space_separated_array.java
@@ -1,3 +1,4 @@
+// normal code for the space separated value addition
 import java.util.Scanner;
 import java.util.Arrays; 
 
@@ -15,6 +16,32 @@ public class Space {
         
         // Use Arrays.toString to see the actual numbers
         System.out.println("Your array: " + Arrays.toString(arr));
+    }
+}
+
+
+// for the case for handling the exception
+public class Space {
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of count want to add: ");
+        int n = sc.nextInt(); 
+        int[] arr = new int[n];
+        
+        System.out.println("Enter " + n + " space-separated integers:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); 
+        }
+        
+        // Use Arrays.toString to see the actual numbers
+        System.out.println("Your array: " + Arrays.toString(arr));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Please enter the valid integer data type elements");
+        }
+        
     }
 }
 
