64ac861df8c74c24a2ca84e6a661d1a0a1781a11
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-25 12:51:58 +0530

    Update space_separated_array.java

--- Java/space_separated_array.java
+++ Java/space_separated_array.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays; 
+
+public class Space {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of count want to add: ");
+        int n = sc.nextInt(); 
+        int[] arr = new int[n];
+        
+        System.out.println("Enter " + n + " space-separated integers:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); 
+        }
+        
+        // Use Arrays.toString to see the actual numbers
+        System.out.println("Your array: " + Arrays.toString(arr));
+    }
+}
 
