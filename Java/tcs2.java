6b9d2b4b6d656b3c78c0628f8292191d997d2ee5
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-28 21:03:02 +0530

    Update tcs2.java

--- Java/tcs2.java
+++ Java/tcs2.java
@@ -1,1 +1,8 @@
-
+// Maximum Product Subset
+// You are given an array of integers (can include negative, zero, positive).
+// Task: Find the maximum product possible by multiplying any subset of elements.
+// Example
+// Input:
+// [-1, 2, 3, 4]
+// Output:
+// 24
