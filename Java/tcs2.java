b860b9c369f21cb574ac9d0eab84702256df1cd8
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-29 19:50:24 +0530

    added code inside tcs2.java

--- Java/tcs2.java
+++ Java/tcs2.java
@@ -6,3 +6,43 @@
 // [-1, 2, 3, 4]
 // Output:
 // 24
+
+
+import java.util.*;
+public class one
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);  
+        System.out.println("Enter number of elements added in the array: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter "+n+" elements in array: ");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array elements are: "+Arrays.toString(arr));
+        
+        
+        int product=1;
+        for(int i=0;i<n;i++)
+        {
+           
+            if(arr[i]>0)
+            {
+                product=product*arr[i];
+
+            }
+            else if (arr[i]==0) {
+                continue;
+            }
+        }
+        System.out.println("Product is: "+product);
+
+    }
+}
+
+
+
