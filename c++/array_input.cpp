c14a170b2b0e4ed15fc94798bb6acdfc8d0c3213
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-03-30 22:32:43 +0530

    add code in array_input.cpp

--- c++/array_input.cpp
+++ c++/array_input.cpp
@@ -1,1 +1,22 @@
-
+// how to take input in array elements
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int n;
+    cout<<"Enter no of elemenst you want to add in array: "<<endl;
+    cin>>n;
+    int num[n];
+    cout<<"Enter "<<n<<" elements in array: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>num[i];
+    }
+    cout<<"Array elements are: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<num[i]<<" ";
+    }
+    return 0;
+}
