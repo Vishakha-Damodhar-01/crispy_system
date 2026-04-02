ce618fcf4a27620e0b036e16b6c1436f2b82ec81
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-02 23:03:02 +0530

    Update add_array_elemet.py

--- Git_Demo/add_array_elemet.py
+++ Git_Demo/add_array_elemet.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 lst = []
-n = int(input("How many elements? "))
+n = int(input("Enter number of elements you want to add in array: "))
 
 for i in range(n):
     lst.append(int(input("Enter element: ")))
