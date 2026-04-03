fe2f85800ece6e017eebe5f339ec7bdfa0c7e26c
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-03 22:28:07 +0530

    add code in speed.cpp with user input

--- c++/speed.cpp
+++ c++/speed.cpp
@@ -1,1 +1,20 @@
+// Question asked in tcs exam
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    float distance, time, speed_ms,speed_kmhr;
+    cout<<"Enter a distance: "<<endl;
+    cin>>distance;
+    cout<<"Enter the time: "<<endl;
+    cin>>time;
+    
+    speed_ms=distance/time;
+    speed_kmhr=speed_ms*3.6;
+    
+    cout<<"Speed in m per sec: "<<speed_ms<<endl;
+    cout<<"Speed in km per hr: "<<speed_kmhr<<endl;
+    
 
+    return 0;
+}
