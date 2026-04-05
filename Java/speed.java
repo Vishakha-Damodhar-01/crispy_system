773d40235de232c720d876613783094291c83c75
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-05 22:45:36 +0530

    Update speed.java

--- Java/speed.java
+++ Java/speed.java
@@ -1,1 +1,17 @@
-
+public class speed
+{
+    public static void main(String[] args) 
+    {
+         float distance,time,msec,kmhr;
+         Scanner sc=new Scanner(System.in);
+         distance=sc.nextFloat();
+         time=sc.nextFloat();
+         
+         msec=distance/time;
+         kmhr=msec*3.6;
+         
+         System.out.println("Speed in meter per second: "+msec);
+         System.out.println("Speed in km per hour: "+kmhr);
+    }
+}
+ 
