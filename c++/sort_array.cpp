8ab2917155bdf45a8613bd4f57595e2ef05555cc
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-08 23:01:55 +0530

    Update sort_array.cpp

--- c++/sort_array.cpp
+++ c++/sort_array.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int n;
+    cout<<"Enter no of elemenst you want to add in array: "<<endl;
+    cin>>n;
+    int num[n];
+    cout<<"Enter "<<n<<" elements in array: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>num[i];
+    }
+    cout<<"Array elements are: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<num[i]<<" ";
+    }
 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    std::sort(arr, arr + n);
+
+    for (int i : arr) std::cout << i << " ";
+    return 0;
+  
+return 0;
+}
