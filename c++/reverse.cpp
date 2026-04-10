29eca8c6aeddc8d9bc7d4d1c8a3dbe11da937c3f
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-10 23:23:23 +0530

    Update reverse.cpp

--- c++/reverse.cpp
+++ c++/reverse.cpp
@@ -1,1 +1,20 @@
+// Rverse of a number in cpp
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,rev=0,r;
+        rev=rev*10+r;
+    cout<<"Enter a number: "<<endl;
+    cin>>n;
+   
+    while(n>0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    cout<<"Reverse "<<" is"<<endl<<rev;
 
+    return 0;
+}
