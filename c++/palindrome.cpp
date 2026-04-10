f5344cba78b8812487ef403ff319d6866ec3a2f8
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-10 23:33:04 +0530

    Update palindrome.cpp

--- c++/palindrome.cpp
+++ c++/palindrome.cpp
@@ -1,1 +1,26 @@
-
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,rev=0,r,temp;
+    cout<<"Enter a number: "<<endl;
+    cin>>n;
+    temp=n;
+    while(n>0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    cout<<"Reverse "<<" is"<<endl<<rev<<endl;
+    
+    if(temp==rev)
+    {
+        cout<<temp<< " is palindrome";
+    }
+    else
+    {
+        cout<<temp<<" is not palindrome";
+    }
+    return 0;
+}
