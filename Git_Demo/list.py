3380db5d46c61b247bac41a378a7de7344492e86
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-11 23:48:22 +0530

    Update list.py

--- Git_Demo/list.py
+++ Git_Demo/list.py
@@ -1,1 +1,9 @@
-
+# How to take user input in the list
+n=int(input("Enter the number of elements need to add: "))
+l=[]
+print("enter",n," elements in l:")
+for i in range(n):
+    x=int(input())
+    l.append(x)
+    
+print(l)
