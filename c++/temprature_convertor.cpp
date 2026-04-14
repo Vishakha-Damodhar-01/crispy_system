073c9ded65962f82219c9dcf35f56d0ed7c10108
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-14 22:03:10 +0530

    Add the cpp code in temprature_convertor.cpp

--- c++/temprature_convertor.cpp
+++ c++/temprature_convertor.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int C,F;
+    float celcius,fahrenheit;
+    cout<<"Enter temp in celcius: "<<endl;
+    cin>>celcius;
+    cout<<"Enter temp in fahrenheit: "<<endl;
+    cin>>fahrenheit;
+    celcius=(F-32)*5/9;
+    fahrenheit=C*9/5+32;
+    cout<<"Celcius to fahrenheit conversion: "<<fahrenheit<<endl;
+    cout<<"fahrenheit to celcius conversion: "<<celcius<<endl;
+    return 0;
+}
