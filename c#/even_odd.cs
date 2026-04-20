df6bf26fade4693f57397c349430617eab35d009
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-20 23:40:10 +0530

    Update even_odd.cs

--- c#/even_odd.cs
+++ c#/even_odd.cs
@@ -1,1 +1,21 @@
 
+using System;
+
+public class HelloWorld 
+{ 
+    public static void Main(string[] args) 
+    { 
+        Console.WriteLine("Enter a number:"); 
+        
+        int num = Convert.ToInt32(Console.ReadLine()); 
+
+        if (num % 2 == 0) 
+        { 
+            Console.WriteLine("Even"); 
+        } 
+        else 
+        { 
+            Console.WriteLine("Odd"); 
+        } 
+    } 
+}
