796e9f488ce18267c84aded4581327eb886866f9
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-22 23:14:19 +0530

    Update prime.cs

--- c#/prime.cs
+++ c#/prime.cs
@@ -1,1 +1,29 @@
 
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+        int count=0;
+        int num=Convert.ToInt32(Console.ReadLine());
+        for(int i=2;i<num/2;i++)
+        {
+            if(num%i==0)
+            {
+                count=1;
+                break;
+            }
+        }
+        
+        if(count==0)
+        {
+            Console.WriteLine("Prime no");
+        }
+        else
+        {
+            Console.WriteLine("Not a prime no");
+        }        
+    }
+}
