b90dd3849253dfbe2743810a5d389bbca6c3cb77
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-23 23:15:04 +0530

    Update fact.cs

--- c#/fact.cs
+++ c#/fact.cs
@@ -1,1 +1,17 @@
+// Write a factorial of a number
+using System;
 
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Enter a number: ");
+        int num=Convert.ToInt32(Console.ReadLine());
+        int fact=1;
+        for(int i=1;i<=num;i++)
+        {
+            fact=fact*i;
+        }
+        Console.WriteLine(fact);
+    }
+}
