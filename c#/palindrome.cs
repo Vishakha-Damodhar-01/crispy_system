18e90ea7414e061c25a2b22b549432181b334cb0
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-04-26 20:01:42 +0530

    Update palindrome.cs

--- c#/palindrome.cs
+++ c#/palindrome.cs
@@ -1,1 +1,29 @@
+using System;
 
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Enter a number: ");
+        int n=Convert.ToInt32(Console.ReadLine());
+        int x=n;
+        int r,rev=0;
+        
+        while(n>0)
+        {
+            r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+        }
+        Console.WriteLine("Reverese "+rev);
+        
+        if(rev==x)
+        {
+            Console.WriteLine("Palindrome no");
+        }
+        else
+        {
+            Console.WriteLine("Non Palindrome no");
+        }
+    }
+}
