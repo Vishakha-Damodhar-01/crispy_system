42375266ef867a2f18284f4b2b6c21bea947facc
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-05-01 23:09:08 +0530

    Update temp_convertor.cs

--- c#/temp_convertor.cs
+++ c#/temp_convertor.cs
@@ -1,1 +1,16 @@
 
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Temprature in Celcius");
+            int celcius=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Temprature in Fahrenheit");
+            int Fahrenheit=Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
