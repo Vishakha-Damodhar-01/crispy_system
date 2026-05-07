16d8c35273f3bf224fb0dbd8937a77717b6e5395
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-05-07 20:31:11 +0530

    Update Encapsulation.cpp

--- c++/Encapsulation.cpp
+++ c++/Encapsulation.cpp
@@ -1,1 +1,40 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+class Teacher
+{
+    private:
+        double salary;
+    
+    public:
+        string name;
+        string dept;
+        string subject;
+        double s;
+        void changeDept(string newDept)
+        {
+            dept=newDept;
+        }
+        
+        void setsalary(double s)
+        {
+            salary=s;
+        }
+        double getsalary()
+        {
+            return salary;
+        }
+};
+int main() {
+    Teacher t1;
+    t1.name="Vishakha";
+    t1.dept="CSE";
+    t1.subject="CND";
+    t1.setsalary(50000);
+
+    cout<<t1.name<<endl;
+    cout<<t1.getsalary()<<endl;
+    
+    return 0;
+}
