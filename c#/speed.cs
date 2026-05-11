9e2fbe45b091c4deab98589ec06563a4fe08e09d
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-05-11 23:31:57 +0530

    Update speed.cs

--- c#/speed.cs
+++ c#/speed.cs
@@ -1,1 +1,18 @@
 
+using System;
+
+public class Speed {
+    public static void Main(string[] args) {
+       
+        Console.WriteLine("Enter distance (meters):"); 
+        float d = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter time (seconds):");
+        float t = float.Parse(Console.ReadLine());
+
+        float div = 18.0f / 5.0f; 
+        float speed = (d / t) * div;
+
+        Console.WriteLine("Speed (km/h): " + speed);
+    }
+}
