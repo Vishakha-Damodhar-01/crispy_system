e740980e899bf04144d9f7d4cf512c1524c347f9
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-05-12 13:03:48 +0530

    Update this_keyword.cpp

--- OOPS/Constructore/this_keyword.cpp
+++ OOPS/Constructore/this_keyword.cpp
@@ -1,1 +1,43 @@
+// We will see the use of *this* keywordin cpp as well as in the constructor
+// Basically its mostly used when , we supposed to use the name same for object creation and the parameters too
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Teacher
+{
+  string name;
+  string dept;
+  string subject;
+
+  private:
+    double salary;
+
+  public:
+    Teacher(string name, string dept,string subject, double salary)
+{
+    // Now here the object and the parameters names are the same
+    // we want to differentiate between objects and parameters
+    // left side-- objects and right side-- parameters
+    // we use pointer (this-->) to the left side
+
+    this-->name=name;
+    this-->dept=dept;
+    this-->subject=subject;
+    this-->salary=salary;
+}
+
+void getInfo()
+{
+  cout<<"Name: "<<name<<endl;
+}
+};
+
+int main()
+{
+  Teacher t1("Vishakha","CSE","CND",50000);
+  t1.getInfo();
+  return 0;
+}
 
