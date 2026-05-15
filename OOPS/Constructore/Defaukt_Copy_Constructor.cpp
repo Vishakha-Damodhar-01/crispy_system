b19655bb80cb799b7fb2f576f5e0c211a39f2d92
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-05-15 15:08:12 +0530

    Update Defaukt_Copy_Constructor.cpp

--- OOPS/Constructore/Defaukt_Copy_Constructor.cpp
+++ OOPS/Constructore/Defaukt_Copy_Constructor.cpp
@@ -31,7 +31,6 @@ class Teacher
         {
             cout<<"Name: "<<name<<endl;
             cout<<"Dept: "<<dept<<endl;
-
         }
 }; 
 //--> its a syntax that class should ended up with semi colon.
@@ -39,11 +38,25 @@ class Teacher
 int main() 
 {
     Teacher t1("Vishakha","CSE","CND",50000);
-    // t1.getInfo();
+    t1.getInfo();
 
+    cout<<"*******"<<endl;
+    
     // whatever we added in the t1 automatically get copied into the t2
   
     Teacher t2(t1);  //--> Default copy constructor get invoke
     t2.getInfo();
     return 0;
 }
+
+/// Output:
+Name: Vishakha
+Dept: CSE
+*******
+Name: Vishakha
+Dept: CSE
+    
+// Here the copy constructor works and when we pass the values from t1 , the values get copied in t2 as well
+// so when we call the t2.getInfo() we get the values from both the objects
+
+// This is how constuctors use in the case of the Data security by creating multiple copies of data in different servers and different locations
