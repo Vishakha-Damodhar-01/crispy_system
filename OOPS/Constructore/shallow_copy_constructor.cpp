2033f7a7a07fbc24577a40d01ff5583fd9a258c3
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-05-27 12:11:05 +0530

    Delete OOPS/Constructore/shallow_copy_constructor.cpp

--- OOPS/Constructore/shallow_copy_constructor.cpp
+++
@@ -1,9 +0,0 @@
-// The shallow copy constructor means copy of a object, like if we have object s1, we can make a copy and paste in s2;
-if we make cout s1 data, change in the s2 data , it got reflected to the s1 too instead of s2 only --> which is drwaback
-
-// as it is it make the copy , if somehow we make changes in s2, its get reflected over the s1 too--> which is the drawback of shallow copy constructor ,
- as it does work on the static memeory like stack not on the dynamic memory like heap.
-
-// let us see how it works with 2 examples
-
-// 1.
