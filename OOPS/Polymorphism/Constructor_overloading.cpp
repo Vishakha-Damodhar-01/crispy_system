32ec7c31507cf52a2babe211eef6c9b2ac2cc3af
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-06-11 23:18:25 +0530

    Update Constructor_overloading.cpp

--- OOPS/Polymorphism/Constructor_overloading.cpp
+++ OOPS/Polymorphism/Constructor_overloading.cpp
@@ -1,1 +1,4 @@
-
+// The constructor overload means, like there are 2 constructors are available 
+//so on the basis of how the data is transfered the constructor called, like if we pass paramters with object reation then paramaertrized constructor called 
+// when we create paramaters later after object creation and then pass it , then non-paramaterized construuctor called
+// let see the examples of it
