7cb7712e03d8258eca807af6148160ad3f2c4907
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-06-13 23:19:13 +0530

    Update function_overriding.cpp

--- OOPS/Polymorphism/Run_time_polymorphism/function_overriding.cpp
+++ OOPS/Polymorphism/Run_time_polymorphism/function_overriding.cpp
@@ -1,2 +1,40 @@
 // Its the concept of inheritance basically 
 // Function overriding means When child class overtake parent class
+//its like the inheritance
+
+#include<iostream>
+#include<string>
+using namespace std;
+class Person
+{
+    public:
+        string name;
+        int age;
+        Person(string name,int age)
+        {
+            this->name=name;
+            this->age=age;
+        }
+        void show()
+        {
+            cout<<"Parent class\n";
+        }
+};
+
+class student:public Person
+{
+
+    void show()
+    {
+        cout<<"child class\n";
+    }
+};
+
+int main()
+{
+    student s1;
+    s1.name="Tanu";
+    s1.age=22;
+    s1.show();
+    return 0;
+}
