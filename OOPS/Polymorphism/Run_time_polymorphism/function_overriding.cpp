9d551ac78047acdc0d55e3646e7dcb5f6c771346
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-06-17 11:44:04 +0530

    Update function_overriding.cpp

--- OOPS/Polymorphism/Run_time_polymorphism/function_overriding.cpp
+++ OOPS/Polymorphism/Run_time_polymorphism/function_overriding.cpp
@@ -5,41 +5,32 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
 class Person
 {
     public:
-        string name;
-        int age;
-        Person(string name,int age)
+        void getInfo()
         {
-            this->name=name;
-            this->age=age;
+            cout<<"Parent class"<<endl;
         }
-        void show()
-        {
-            cout<<"Parent class\n";
-        }
-};
 
+};
 class student:public Person
 {
-
-    void show()
-    {
-        cout<<"child class\n";
-        cout<<Name<<" "<<age;
-    }
+    public:
+        void getInfo()
+        {
+            cout<<"Child class"<<endl;
+        }
+        
 };
-
 int main()
 {
     student s1;
-    s1.name="Tanu";
-    s1.age=22;
-    s1.show();
+    s1.getInfo();
     return 0;
 }
 
-// output:
-child class
-Tanu 22
+
+//output:
+Child class
