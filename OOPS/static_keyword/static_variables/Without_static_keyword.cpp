2b1cedc06c63f902db28aff8cf8846e6906fbe5a
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-06-19 23:36:54 +0530

    Update Without_static_keyword.cpp

--- OOPS/static_keyword/static_variables/Without_static_keyword.cpp
+++ OOPS/static_keyword/static_variables/Without_static_keyword.cpp
@@ -1,8 +1,11 @@
+// Without static key word
+
 #include<iostream>
 #include<string>
 using namespace std;
 
 // without static value the "value of X" is not updated
+
 void fun()
 {
     int x=0;
@@ -16,3 +19,13 @@ int main()
     fun();
     return 0;
 }
+
+// output :
+x=: 0
+x=: 0
+x=: 0
+  --> The output remains the same , because it works in the stack like 
+       when we satrt the program one value push, again it pull
+        again called--->push -->again -->pop
+
+  that's why value remains the same
