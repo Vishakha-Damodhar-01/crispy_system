0692cf67c9c906abcdca4fd409887b05e8bf5b38
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-06-22 23:52:49 +0530

    Update algebric_exp.cpp

--- c++/algebric_exp.cpp
+++ c++/algebric_exp.cpp
@@ -3,9 +3,18 @@
 using namspace std;
 int main()
 {
+  cout<<"enter a: "<<endl;
   cin>>a;
+  cout<<"enter a: "<<endl;
   cin>>b;
   ans=a+b;
+  cout<<"ans: "<<ans<<endl;
   return0;
 }
   
+//output:
+enter a:
+5
+enter b:
+8
+ans: 13
