c94756e5a0e8a122f931e75bdd392725b46b03eb
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-06-24 23:09:45 +0530

    Create algebric_exp.cs

---
+++ c#/algebric_exp.cs
@@ -0,0 +1,21 @@
+// WE will perform the algebric expressions using c#
+using System;
+
+public class HelloWorld 
+{ 
+    public static void Main(string[] args) 
+    { 
+        Console.WriteLine("Enter a number:"); 
+        
+        int num = Convert.ToInt32(Console.ReadLine()); 
+
+        if (num % 2 == 0) 
+        { 
+            Console.WriteLine("Even"); 
+        } 
+        else 
+        { 
+            Console.WriteLine("Odd"); 
+        } 
+    } 
+}
