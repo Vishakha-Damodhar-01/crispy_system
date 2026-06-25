4807a322b2e9c7a40a5dceabce89c91844247e40
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-06-25 23:32:30 +0530

    Update algebric_exp.cs

--- c#/algebric_exp.cs
+++ c#/algebric_exp.cs
@@ -2,20 +2,24 @@
 using System;
 
 public class HelloWorld 
-{ 
+{
     public static void Main(string[] args) 
-    { 
+    {
         Console.WriteLine("Enter a:");
-        int a = Convert.ToInt32(Console.ReadLine()); 
+        int a = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("Enter b:");
-        int b = Convert.ToInt32(Console.ReadLine()); 
-        int add=a+b;
-        Console.WriteLine("Addition: ", add);
-        int sub=a-b;
-        Console.WriteLine("Subtraction: ", sub);
-        int mul=a*b;
-        Console.WriteLine("Multiplication: ", mul);
-    } 
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int add = a + b;
+        Console.WriteLine($"Addition: {add}");
+
+        int sub = a - b;
+        Console.WriteLine($"Subtraction: {sub}");
+
+        int mul = a * b;
+        Console.WriteLine($"Multiplication: {mul}");
+    }
 }
 
 //output:
