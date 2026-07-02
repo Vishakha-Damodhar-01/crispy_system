ce43c1564a39501742acbf8484d5090c82ceec4b
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-07-02 23:46:46 +0530

    Update Algebric_expression.java

--- Java/Algebric_expression.java
+++ Java/Algebric_expression.java
@@ -1,1 +1,18 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a=sc.nextInt();
+        System.out.println("Enter b: ");
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("Addition is: "+c);
+    }
+}
 
+Enter a: 
+66
+Enter b: 
+45
+Addition is: 111
