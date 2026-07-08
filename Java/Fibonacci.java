dcd10e2556d98b615a085b9bdfa6e6bb8f025833
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-07-08 23:22:39 +0530

    Update Fibonacci.java

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -8,9 +8,10 @@ class Main {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number till which you want the fibonacci series...");
         int n=sc.nextInt();
+        System.out.println("The Fibonacci series will be: ");
         for(int v=0;v<=n;v++)
         {
-            System.out.println(i+" ");
+            System.out.print(i+" ");
             int m=i+j;
             i=j;
             j=m;
@@ -23,6 +24,7 @@ class Main {
 The fibonnaci series will be: 
 Enter a number till which you want the fibonacci series...
 5
+    
 0 
 1 
 1 
