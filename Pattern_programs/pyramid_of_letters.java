2ca94f41c31c54f299fe53805b9cae22159d1e00
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-07-10 23:21:10 +0530

    Update pyramid_of_letters.java

--- Pattern_programs/pyramid_of_letters.java
+++ Pattern_programs/pyramid_of_letters.java
@@ -1,4 +1,5 @@
 //Here we can make the multiple patterns of the name of the letters too
+//1. 
 public class pattern
 {
     public static void main(String[] args)
@@ -15,6 +16,23 @@ public class pattern
     }
 }
 
+//2..//same program with differnt logic
+public class pattern
+{
+    public static void main(String[] args)
+    {
+        int n=6;
+        for(int i=6;i>=1;i--)
+        {
+            for(int j=6;j>=i;j--)
+            {
+                System.out.print("Vish"+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
 //output:
 Vish	
 Vish	Vish	
@@ -22,3 +40,4 @@ Vish	Vish	Vish
 Vish	Vish	Vish	Vish	
 Vish	Vish	Vish	Vish	Vish	
 Vish	Vish	Vish	Vish	Vish	Vish
+
