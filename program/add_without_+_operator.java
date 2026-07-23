05b87114877bc7b7c9d67b07613bf52cb9881629
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-07-23 23:54:26 +0530

    Update add_without_+_operator.java

--- program/add_without_+_operator.java
+++ program/add_without_+_operator.java
@@ -1,1 +1,15 @@
 
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a=sc.nextInt();
+        System.out.println("Enter b:");
+        int b=sc.nextInt();
+        float c=a-(-b);
+        System.out.println("Enter c:");
+        
+    }
+}
