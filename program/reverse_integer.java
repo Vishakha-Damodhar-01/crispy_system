d720a48947d8c063a9546ef87ac0fbc854fa07a7
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-07-24 23:49:15 +0530

    Create reverse_integer.java

---
+++ program/reverse_integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        int r;
+        int rev=0;
+        while(x>0)
+        {
+            r=x%10;
+            rev=rev*10+r;
+            x=x/10;
+        }
+        return rev;
+    }
+}
