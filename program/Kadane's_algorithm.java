8a0bdf7508a8c64598253ee9e1c2084f8ab8f8fc
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-07-25 23:58:14 +0530

    Update Kadane's_algorithm.java

--- program/Kadane's_algorithm.java
+++ program/Kadane's_algorithm.java
@@ -1,1 +1,26 @@
 
+public class KadanesAlgorithm {
+    public static int[] findMaxSubarrayBounds(int[] nums) {
+    int maxSum = nums[0];
+    int curSum = 0;
+    int maxL = 0, maxR = 0;
+    int L = 0;
+
+    for (int R = 0; R < nums.length; R++) {
+        if (curSum < 0) {
+            curSum = 0;
+            L = R; // Reset the left window index forward
+        }
+        curSum += nums[R];
+
+        if (curSum > maxSum) {
+            maxSum = curSum;
+            maxL = L;
+            maxR = R;
+        }
+    }
+    // Returns [start_index, end_index]
+    return new int[] {maxL, maxR};
+}
+
+}
