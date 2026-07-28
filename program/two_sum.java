dd9df94882a7af52e1c95438d50c5df60b62dccb
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-07-28 23:56:05 +0530

    Update two_sum.java

--- program/two_sum.java
+++ program/two_sum.java
@@ -1,1 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Map to store the number as the key and its index as the value
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            // If the complement exists in the map, we found our pair
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            
+            // Otherwise, store the current number and its index
+            map.put(nums[i], i);
+        }
+        
+        // Return an empty array if no solution is found
+        return new int[] {};
+    }
+}
 
