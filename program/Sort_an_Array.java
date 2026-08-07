0f012889811ce2bb6ecc8452b00fb1114837c9f4
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-08-07 23:31:56 +0530

    Create Sort_an_Array.java

---
+++ program/Sort_an_Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+                {
+
+            }
+        }
+        
+        return nums;
+    }
+}
