e94eb6a880b2f96896b8e2f92faa5e1d2186f0c4
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-08-10 23:39:18 +0530

    Update Remove_duplicate_from_sorted_array.java

--- program/Array/Remove_duplicate_from_sorted_array.java
+++ program/Array/Remove_duplicate_from_sorted_array.java
@@ -1,1 +1,16 @@
 
+default boiler plate of problem
+
+  
+  int i=0;
+for(int j=1;j<nums.length;j++)
+  {
+    if(num[i]!=num[j])
+    {
+      num[i+1]=num[j];
+      i++;
+    }
+  }
+return i+1;
+
+this is how it returns the number of counts of uniques elements
