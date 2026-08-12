02b47a254d897099e19830b59960aa1febbf89c9
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-08-12 23:48:52 +0530

    Update Largest_Element.java

--- program/Array/Largest_Element.java
+++ program/Array/Largest_Element.java
@@ -1,1 +1,21 @@
 
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int elements=nums.length;
+        int valcount=0;
+        int count;
+        for(int i=0;i<nums.length;i++)
+        {
+           if(nums[i]==val)
+           {
+            valcount+=1;
+           }
+           else
+           {
+            i++;
+           }
+        }
+        count=elements-valcount;
+        return count;
+    }
+}
