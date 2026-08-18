c271bf9361b1360c6d4c3f1dd8eb3a6fd2913937
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-08-18 23:52:38 +0530

    Update Valid_anagram.java

--- program/sorting/Valid_anagram.java
+++ program/sorting/Valid_anagram.java
@@ -23,3 +23,17 @@ class Solution {
         return true;
     }
 }
+
+
+//output:
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
