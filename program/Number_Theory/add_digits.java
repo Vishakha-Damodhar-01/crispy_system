7ec02550e432a71c3fa995ca92175cd9af417323
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-09-05 23:30:26 +0530

    Update add_digits.java

--- program/Number_Theory/add_digits.java
+++ program/Number_Theory/add_digits.java
@@ -1,3 +1,20 @@
+//This is the case from which you are able to solve the problem
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10; // Extract the last digit
+                num /= 10;       // Remove the last digit
+            }
+            num = sum; // Update num with the sum of its digits
+        }
+        return num;
+    }
+}
+
+
+//This is the one case
 class Solution {
     public int addDigits(int num) {
         int r;
