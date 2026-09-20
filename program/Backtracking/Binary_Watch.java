410f3e5281c7a1df78fd2380484cbbaf1dadeccd
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-09-20 23:26:32 +0530

    Added code

--- program/Backtracking/Binary_Watch.java
+++ program/Backtracking/Binary_Watch.java
@@ -1,1 +1,23 @@
+//Problem no:401 --> Binary watch
+import java.util.ArrayList;
+import java.util.List;
 
+public class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+        
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    if (m < 10) {
+                        result.add(h + ":0" + m);
+                    } else {
+                        result.add(h + ":" + m);
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
