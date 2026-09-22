b3ad025de2c2411e37b021d66eb0e53344fdf5db
Vishakha Bhagwan Damodhar <141665258+Vishakha-Damodhar-01@users.noreply.github.com>
2026-09-22 23:16:04 +0530

    Create missing_number.java

---
+++ program/Binary_Tree/missing_number.java
@@ -0,0 +1,11 @@
+//Problem 268: Binary Tree --> Missing number
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+}
